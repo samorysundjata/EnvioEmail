ee5bef121675f7905526f44e7ea0636aa5d0bab6
Samory Sundjata <samory.sundjata@gmail.com>
2021-05-10 18:21:09 -0300

    Implementando a classe de envio de e-mail.

---
+++ EnvioEmail/EmailServico.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace EnvioEmail
+{
+    public class EmailServico : MailMessage
+    {
+        #region Private Fields
+
+        private readonly string EMAIL_HOST = ConfigurationManager.AppSettings["emailServico:email_host"];
+        private readonly string EMAIL_ORIGEM = ConfigurationManager.AppSettings["emailServico:email_remetente"];
+        private readonly string EMAIL_SENHA = ConfigurationManager.AppSettings["emailServico:email_senha"];
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        public void MandarEmail(MailMessage mensagem)
+        {
+            try
+            {
+                using (var mensagemEmail = new MailMessage())
+                {
+                    mensagemEmail.From = new MailAddress(EMAIL_ORIGEM);
+                    mensagemEmail.To.Add(new MailAddress(mensagem.To.ToString()));
+
+                    mensagemEmail.Subject = mensagem.Subject;
+                    mensagemEmail.Body = mensagem.Body;
+                    mensagemEmail.BodyEncoding = Encoding.UTF8;
+                    mensagemEmail.BodyEncoding = Encoding.GetEncoding("ISO-8859-1");
+                    mensagemEmail.Priority = MailPriority.Normal;
+
+                    using (var smtpCliente = new SmtpClient())
+                    {
+                        smtpCliente.Host = EMAIL_HOST;
+                        smtpCliente.Port = 587;
+                        smtpCliente.EnableSsl = true;
+                        smtpCliente.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        smtpCliente.Credentials = new NetworkCredential(EMAIL_ORIGEM, EMAIL_SENHA);
+                        smtpCliente.UseDefaultCredentials = false;
+
+                        smtpCliente.Send(mensagemEmail);
+                    }
+                }
+            }
+            catch (SmtpFailedRecipientException ex)
+            {
+                Console.WriteLine("Mensagem : {0} " + ex.Message);
+                return;
+            }
+            catch (SmtpException ex)
+            {
+                Console.WriteLine("Mensagem SMPT Fail : {0} " + ex.Message);
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Mensagem Exception : {0} " + ex.Message);
+                return;
+            }
+
+            Console.WriteLine("Email enviado com sucesso!");
+            Console.ReadKey();
+        }
+
+        #endregion Public Methods
+    }
+}
--- EnvioEmail/Program.cs
+++ EnvioEmail/Program.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.Net.Mail;
 
 namespace EnvioEmail
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        #region Private Methods
+
+        private static void EscreverMensagem()
         {
-            Console.WriteLine("Envio de email.");
-            Console.ReadKey();
+            using (var mensagem = new MailMessage())
+            {
+                Console.WriteLine("Escreva o seu e-mail");
+                string remetente = Console.ReadLine();
+                mensagem.To.Add(remetente);
+                Console.WriteLine("Escreva o assunto do email");
+                mensagem.Subject = Console.ReadLine();
+                Console.WriteLine("Escreva a sua mensagem");
+                mensagem.Body = Console.ReadLine();
+
+                using (var emailManda = new EmailServico())
+                {
+                    emailManda.MandarEmail(mensagem);
+                }
+            }
         }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Este é o envio de mail, se quiser mandar uma mensagem digite 'Mandar'");
+
+            var chave = Console.ReadLine();
+
+            if (chave == "Mandar")
+                EscreverMensagem();
+            else
+                Console.WriteLine("Seu comando foi incorreto, reinicie a aplicação."); 
+        }
+
+        #endregion Private Methods
     }
 }
